ec6537ae7f8671eb3d2a89f9c6f736cfeeaf2069
Bob Zhu <61638801+Bob-Zhu3@users.noreply.github.com>
2024-04-06 13:55:10 -0700

    Update main.py
    Game window

--- src/client/main.py
+++ src/client/main.py
@@ -1,21 +1,27 @@
-import socket
+# Example file showing a basic pygame "game loop"
+import pygame
 
-from utils import network
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
 
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-def main():
-    # test
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("", 12345))
-    obj = {
-        "test": [1, 2, 3],
-        "test2": True,
-    }
-    print("obj:", obj)
-    network.send(sock, obj)
-    print("recv:", network.recv(sock))
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
 
+    # RENDER YOUR GAME HERE
 
+    # flip() the display to put your work on screen
+    pygame.display.flip()
 
-if __name__ == "__main__":
-    main()
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
